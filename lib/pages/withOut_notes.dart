47517dce340855eac52fe77f25755d82799c9bdc
Nurtilek <abdlhtv06@gmail.com>
2026-07-08 11:02:31 +0300

    пейж когда нет заметок

---
+++ lib/pages/withOut_notes.dart
@@ -0,0 +1,98 @@
+import 'package:bloctestapp/pages/got_card.dart';
+import 'package:bloctestapp/widgets/cards_in_mainpages.dart';
+import 'package:bloctestapp/widgets/show_diolog.dart';
+import 'package:flutter/material.dart';
+
+class WithOutNotes extends StatelessWidget {
+  const WithOutNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: .center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(30),
+                    margin: EdgeInsets.only(bottom: 25),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0.5, color: Colors.grey),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.04),
+                          blurRadius: 16,
+                          offset: const Offset(0, 2),
+                        ),
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.02),
+                          blurRadius: 4,
+                          offset: const Offset(0, 1),
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(28.0),
+                    ),
+                    height: 100,
+                    width: 100,
+                    child: Image.asset(
+                      'assets/icons/note.png',
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    'Пока нет заметок',
+                    style: TextStyle(fontSize: 23, fontWeight: FontWeight(700)),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      'Нажмите <<+>> ,чтобы создать первую заметку и навести порядок в мыслях',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight(400),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Positioned(
+              bottom: 25,
+              right: 0,
+              child: InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => Showdiolog(),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Theme.of(
+                      context,
+                    ).floatingActionButtonTheme.backgroundColor,
+                  ),
+                  height: 70,
+                  width: 70,
+                  child: Center(
+                    child: Icon(Icons.add, color: Colors.white, size: 35),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
