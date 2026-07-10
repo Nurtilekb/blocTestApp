e5d991df15ce60abb1bc67339e11c9a9162c82ff
Nurtilek <abdlhtv06@gmail.com>
2026-07-10 15:30:10 +0300

    commented some search code

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,8 +1,10 @@
+import 'package:bloctestapp/bloc/notes_bloc.dart';
 import 'package:bloctestapp/models/card_manager.dart';
 import 'package:bloctestapp/models/user.dart';
 import 'package:bloctestapp/widgets/app_input_widget.dart';
 import 'package:bloctestapp/widgets/cards_in_mainpages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -24,39 +26,39 @@ class _SearchPageState extends State<SearchPage> {
     super.dispose();
   }
 
-  List<String> get _categories {
-    final cards = _cardManager.getAllCards();
-    return cards.map((c) => c.category).toSet().toList();
-  }
+  // List<String> get _categories {
+  //   final cards = _cardManager.getAllCards();
+  //   return cards.map((c) => c.category).toSet().toList();
+  // }
 
-  List<Notes> get _filteredCards {
-    var cards = _cardManager.getAllCards();
+  // List<Notes> get _filteredCards {
+  //   var cards = _cardManager.getAllCards();
 
-    // Фильтр по поиску
-    if (_searchQuery.isNotEmpty) {
-      cards = cards
-          .where(
-            (card) =>
-                card.title.toLowerCase().contains(_searchQuery.toLowerCase()) ||
-                card.description.toLowerCase().contains(
-                  _searchQuery.toLowerCase(),
-                ),
-          )
-          .toList();
-    }
+  //   // Фильтр по поиску
+  //   if (_searchQuery.isNotEmpty) {
+  //     cards = cards
+  //         .where(
+  //           (card) =>
+  //               card.title.toLowerCase().contains(_searchQuery.toLowerCase()) ||
+  //               card.description.toLowerCase().contains(
+  //                 _searchQuery.toLowerCase(),
+  //               ),
+  //         )
+  //         .toList();
+  //   }
 
-    // Фильтр по категории
-    if (_selectedCategory != null) {
-      cards = cards
-          .where((card) => card.category == _selectedCategory)
-          .toList();
-    }
+  //   // Фильтр по категории
+  //   if (_selectedCategory != null) {
+  //     cards = cards
+  //         .where((card) => card.category == _selectedCategory)
+  //         .toList();
+  //   }
 
-    // Сортировка по дате (новые сверху)
-    cards.sort((a, b) => b.date.compareTo(a.date));
+  //   // Сортировка по дате (новые сверху)
+  //   cards.sort((a, b) => b.date.compareTo(a.date));
 
-    return cards;
-  }
+  //   return cards;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -101,19 +103,38 @@ class _SearchPageState extends State<SearchPage> {
               const SizedBox(height: 16),
               Expanded(
                 child: SizedBox(
-                  child: ListView.separated(
-                    itemCount: _filteredCards.length,
-                    itemBuilder: (context, index) {
-                      final card = _filteredCards[index];
-                      return CardsInPage(
-                        mainText: card.title,
-                        descripText: card.description,
-                        dateTime: ("${card.date}"),
-                        categoryText: card.category,
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return const SizedBox(height: 12);
+                  child: BlocBuilder<NotesBloc, NotesState>(
+                    builder: (context, state) {
+                      if (_searchQuery == null || _searchQuery.isEmpty) {
+                        return SizedBox(
+                          child: Center(child: Text('Поиск заметок...')),
+                        );
+                      } else
+                        return BlocBuilder<NotesBloc, NotesState>(
+                          builder: (context, state) {
+                            if (state is! NotesLoaded) {
+                              return const SizedBox();
+                            }
+
+                            final cards = state.notes;
+
+                            return ListView.separated(
+                              itemCount: cards.length,
+                              itemBuilder: (context, index) {
+                                return CardsInPage(
+                                  mainText: cards[index].title,
+                                  descripText: cards[index].description,
+                                  dateTime: ("${cards[index].date}"),
+                                  categoryText: cards[index].category,
+                                  detterId: cards[index].id,
+                                );
+                              },
+                              separatorBuilder: (context, index) {
+                                return const SizedBox(height: 12);
+                              },
+                            );
+                          },
+                        );
                     },
                   ),
                 ),
