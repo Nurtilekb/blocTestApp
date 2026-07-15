984f5cb4b00a246a561717d9b8acaf0cfed00f06
Nurtilek <abdlhtv06@gmail.com>
2026-07-15 15:59:01 +0300

    defoult categories import from maindart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bloctestapp/bloc/notes_bloc.dart';
 import 'package:bloctestapp/bloc/repositories/notes_repository.dart';
+import 'package:bloctestapp/models/category_model.dart';
 import 'package:bloctestapp/services/card_manager.dart';
 import 'package:bloctestapp/models/note.dart';
 import 'package:bloctestapp/pages/hompage/home_page.dart';
@@ -12,9 +13,22 @@ import 'theme/light.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  Hive.registerAdapter(NotesAdapter()); // Регистрируем адаптер
+  Hive.registerAdapter(NotesAdapter());
+  Hive.registerAdapter(CategoryModelAdapter());
+  await Hive.openBox<Notes>('notesBox');
+  final categoriesBox = await Hive.openBox<CategoryModel>('categoriesBox');
 
-  await Hive.openBox<Notes>('cardsBox');
+  if (categoriesBox.isEmpty) {
+    final defaults = [
+      CategoryModel(id: 0, name: 'Личное'),
+      CategoryModel(id: 1, name: 'Работа'),
+      CategoryModel(id: 2, name: 'Идеи'),
+      CategoryModel(id: 3, name: 'Важное'),
+    ];
+    for (final cat in defaults) {
+      await categoriesBox.put(cat.id, cat);
+    }
+  }
 
   runApp(MyApp());
 }
